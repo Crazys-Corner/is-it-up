e493f9415060a78cbb6d7c51238e8d80fa73120d
Crazys-Corner <97609827+Crazys-Corner@users.noreply.github.com>
2022-04-22 09:07:25 -0400

    Update serverchecker.go

--- serverchecker.go
+++ serverchecker.go
@@ -1,18 +1,22 @@
 package main
-
 import (
     "fmt"
     "net"
+    "os"
 )
-
 func main() {
+        ip:= os.Args[1]
+        port := os.Args[2]
 
-    conn, err := net.Dial("tcp", "78.108.218.23:25555")
+        ipandport := ip+":"+port
+
+
+    conn, err := net.Dial("tcp", ipandport, )
     if err != nil {
         fmt.Println("Server is Down :C, L bozo ratio")
         return
     } else {
-        fmt.Println("Server is Up!")
+        fmt.Println(ipandport, "is online!")
     }
     defer conn.Close()
 }
